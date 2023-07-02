8326366f063e4299398823f3ac42d383eadb992d
Piumal Dhanawardhana <24piumal@gmail.com>
2023-07-02 17:21:59 +0530

    fix errors in contact-controller's put ndelete

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -32,38 +32,58 @@ const createContact = async (req, res) => {
 
 //Update contact
 const updateContact = async (req, res) => {
-  try {
+  
+  try {   
+  const contact = await contacts.findById(req.params.id);
 
-    if (contact.user_id.toString() !== req.user.id) { //if the user id is not equal to the user id in the request then throw error
-      res.status(403);
-      throw new Error("User don't have permission to update other user contacts");
-    }
-      const updateContact = await contacts.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      }
-    );
-    res.status(200).json(updateContact);
-    } catch (err) {
-      res.json(err);
-  };
+  if (!contact) { //if contact not found
+    res.status(404);
+    res.json("Contact not found");
+  }
+
+  console.log(contact.user_id.toString());
+  console.log(req.body.userId);
+
+  if (contact.user_id.toString() !== req.body.userId) { //if the user id is not equal to the user id in the request then throw error
+    res.status(403);
+    res.json("User don't have permission to update other user contacts");
+  }
+
+  const updateContact = await contacts.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    });
 
   res.status(200).json(updateContact);
-};
+}
+catch (err) { 
+  res.json(err);
+}
+}
 
 //Delete contact
 const deleteContact = async (req, res) => {
+
   try {
-    if (contact.user_id.toString() !== req.user.id) { //if the user id is not equal to the user id in the request then throw error
-      res.status(403);
-      throw new Error("User don't have permission to update other user contacts");
+    const contact = await contacts.findById(req.params.id);
+
+    if (!contact) { //if contact not found
+      res.status(404);
+      res.json("Contact not found");
     }
+
+    if (contact.user_id.toString() !== req.body.userId) { //if the user id is not equal to the user id in the request then throw error
+      res.status(403);
+      res.json("User don't have permission to update other user contacts");
+    }
+
     //find the item by its id and delete it
     const deleteExpense = await contacts.findByIdAndDelete(
       req.params.id
     );
     res.status(200).json("contact Deleted");
+    // console.log("contact Deleted")
   } catch (err) {
     res.json(err);
   }
